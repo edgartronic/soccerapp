b73118e2bad708d334a96cf3f4d2a2e498345192
Edgar Nunez <edgar.a.nunez@gmail.com>
2013-07-11 14:45:55 -0700

    Adding STConstants header file to store all defines. Including in prefix header so it’s accessible app-wide.

---
+++ soccertrainer/soccertrainer/STConstants.h
@@ -0,0 +1,24 @@
+//
+//  STConstants.h
+//  soccertrainer
+//
+//  Created by Edgar Nunez on 7/11/13.
+//  Copyright (c) 2013 Edgar Nunez. All rights reserved.
+//
+
+#ifndef soccertrainer_STConstants_h
+#define soccertrainer_STConstants_h
+
+
+#define PLAYER_NAME @"playerName"
+#define PLAYER_NAME_FIRST @"playerNameFirst"
+#define PLAYER_NAME_LAST @"playerNameLast"
+#define PLAYER_ADDRESS_ONE @"playerAddressOne"
+#define PLAYER_ADDRESS_TWO @"playerAddressTwo"
+#define PLAYER_ADDRESS_CITY @"playerAddressCity"
+#define PLAYER_ADDRESS_STATE @"playerAddressState"
+#define PLAYER_ADDRESS_ZIP @"playerAddressZip"
+#define PLAYER_AGE @"playerAge"
+
+
+#endif
